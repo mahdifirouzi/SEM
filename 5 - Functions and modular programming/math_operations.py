094cac027ba1b30048232a6cc84fe7698ee9f0af
Mahdi Firouzi <mahdi.firouzi.n@gmail.com>
2024-01-19 11:21:18 +0000

    Cleanup

--- 5 - Functions and modular programming/math_operations.py
+++
@@ -1,9 +0,0 @@
-# math_operations.py
-
-def add(a, b):
-# Addition operation.
-    return a + b
-
-def subtract(a, b):
-# Subtraction operation.
-    return a - b
--- 5 - Functions and modular programming/my_package/__init__.py
+++
@@ -1,8 +0,0 @@
-# main_script __init__.py
-from my_package import math_operations
-
-result_add = math_operations.add(5, 3)
-result_subtract = math_operations.subtract(8, 2)
-
-print("Addition:", result_add)
-print("Subtraction:", result_subtract)
